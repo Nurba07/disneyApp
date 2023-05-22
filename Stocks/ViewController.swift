62ae5ceb22f8dd2c3dcab2917a07a5bc0c67dd5c
Nurba72 <nurbanauryzbay@gmail.com>
2023-05-22 22:01:19 +0600

    UIsearchBar added

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    
     let baseUrl = "https://api.disneyapi.dev/character"
+    
+    var searchActive: Bool = false
     
     let defaultSession = URLSession(configuration: .default)
     var dataTask: URLSessionDataTask?
@@ -24,12 +27,28 @@ class ViewController: UIViewController {
         }
     }
 
+    var filteredCharacter: [GetData] = [GetData]() {
+        didSet{
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        lazy var searchBar: UISearchBar = UISearchBar(frame: CGRectMake(0, 0, view.frame.width, 20))
+
         getData()
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
         tableView.register(StockTableViewCell.nib, forCellReuseIdentifier: StockTableViewCell.identifier)
+        searchBar.placeholder = "texttest"
+        var leftBar = UIBarButtonItem(customView: searchBar)
+        self.navigationItem.rightBarButtonItem = leftBar
+        self.filteredCharacter = character
+
         // Do any additional setup after loading the view.
     }
     
@@ -55,19 +74,43 @@ class ViewController: UIViewController {
 
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension ViewController: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return character.count
+        var counter: Int
+        if(searchActive){
+            counter = filteredCharacter.count
+        }else {
+            counter = character.count
+
+        }
+        return counter
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: StockTableViewCell.identifier) as! StockTableViewCell
-        cell.nameLabel.text = character[indexPath.row].name
-        let url = URL(string: character[indexPath.row].imageUrl)
-        print(url)
-        cell.characterImage.kf.setImage(with: url)
+        if (searchActive == true) {
+            cell.nameLabel.text = filteredCharacter[indexPath.row].name
+            let url = URL(string: filteredCharacter[indexPath.row].imageUrl)
+            cell.characterImage.kf.setImage(with: url)
+        } else {
+            cell.nameLabel.text = character[indexPath.row].name
+            let url = URL(string: character[indexPath.row].imageUrl)
+            cell.characterImage.kf.setImage(with: url)
+        }
         return cell
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.filteredCharacter = character.filter { $0.name == searchText }
+        print(self.filteredCharacter.count)
+        if(filteredCharacter.count == 0){
+            self.searchActive = false
+        }else{
+            self.searchActive = true
+        }
+        self.tableView.reloadData()
+    }
+
+
 }
